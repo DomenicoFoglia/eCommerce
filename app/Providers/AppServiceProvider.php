2e23f30f060daecbedcde2973b8e9a65e27daf17
DomenicoFoglia <foglia.dmnc@gmail.com>
2025-09-15 17:50:03 +0200

    Aggiunti e gestiti link Home, Registrati, Login e Logout nella navbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,25 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        // Dopo il login redirect alla homepage
-        $this->app->singleton(LoginResponse::class, function () {
-            return new class implements LoginResponse {
-                public function toResponse($request)
-                {
-                    return redirect()->route('homepage');
-                }
-            };
-        });
-
-        // Dopo la registrazione redirect alla homepage
-        $this->app->singleton(RegisterResponse::class, function () {
-            return new class implements RegisterResponse {
-                public function toResponse($request)
-                {
-                    return redirect()->route('homepage');
-                }
-            };
-        });
+        //
     }
 
     /**
