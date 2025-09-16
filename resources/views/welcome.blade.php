8c5edad14c74684a1fdc3f76a63337085ea994c3
DomenicoFoglia <foglia.dmnc@gmail.com>
2025-09-16 23:22:51 +0200

    Create card per i prodotti; Ultimi sei prodotti inseriti ora visibili sulla homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,20 @@
                 <div class="my-3">
                     @auth
                         <a href="{{ route('create.article') }}" class="btn btn-dark">Crea un articolo</a>
-                    </div>
-                @endauth
+                    @endauth
+                </div>
             </div>
+        </div>
+        <div class="row justify-content-center align-items-center py-5">
+            @forelse ($articles as $article)
+                <div class="col-12 col-md-3">
+                    <x-card :article="$article" />
+                </div>
+            @empty
+                <div class="col-12">
+                    <h3 class="text-center">Non sono stati creati ancora articoli</h3>
+                </div>
+            @endforelse
         </div>
     </div>
 </x-layout>
